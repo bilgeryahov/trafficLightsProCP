fe43af9a10af483feb9eee671bc79a5737ea448f
Critical187 <chishi@abv.bg>
2016-03-30 14:55:30 +0200

    Add extra methods...again

--- Code/TrafficLights/TrafficLights/TrafficManager.cs
+++ Code/TrafficLights/TrafficLights/TrafficManager.cs
@@ -30,5 +30,60 @@ namespace TrafficLights
             return new Simulation(this.Grid);
         }
 
+        public void IncreaseSimulationSpeed()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DecreaseSimulationSpeed()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void StartSimulation()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void StopSimulation()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PauseSimulation()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ResumeSimulation()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void FinishSimulation()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RestartSimulation()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void RemoveCrossing()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SaveSimulation()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void LoadFromFile()
+        {
+            throw new System.NotImplementedException();
+        }
+
     }
 }
